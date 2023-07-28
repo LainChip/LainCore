00be3f1ff06903a92154a2de0f611f2c72756d49
dofingert <dofingert@gmail.com>
2023-07-28 14:23:32 +0800

    通过到 n37

--- rtl/utils/muler_32x32.sv
+++ rtl/utils/muler_32x32.sv
@@ -1,53 +1,53 @@
 `include "decoder.svh"
 
 module muler_32x32(
-    input wire clk,
-    input wire rst_n,
-    input wire[1:0] op_i,
-    input wire ex_stall_i,
-    input wire m1_stall_i,
-    input wire m2_stall_i,
-    input wire[31:0] r0_i,
-    input wire[31:0] r1_i,
-    output logic[31:0] result_o
-  );
+  input wire clk,
+  input wire rst_n,
+  input wire[1:0] op_i,
+  input wire ex_stall_i,
+  input wire m1_stall_i,
+  input wire m2_stall_i,
+  input wire[31:0] r0_i,
+  input wire[31:0] r1_i,
+  output logic[31:0] result_o
+);
 
-  logic signed_ext;
-  assign signed_ext = op_i == `_MUL_TYPE_MULH;
-  logic get_high, get_high_q;
-  assign get_high = op_i == `_MUL_TYPE_MULH || op_i == `_MUL_TYPE_MULHU;
+logic signed_ext;
+assign signed_ext = op_i == `_MUL_TYPE_MULH;
+logic get_high, get_high_m1, get_high_q;
+assign get_high = op_i == `_MUL_TYPE_MULH || op_i == `_MUL_TYPE_MULHU;
 
-  logic[48:0] l_q;
-  logic[63:16] h_q;
+logic[31:0] a;
+logic[31:0] b;
+assign a = r0_i;
+assign b = r1_i;
+logic[63:32] h_fix;
+assign h_fix = ((a[31] & signed_ext) ? -b : '0) +
+  ((b[31] & signed_ext) ? -a : '0);
 
-  logic[32:0] a;
-  logic[32:0] b;
-  always_comb begin
-    a[31:0] = r0_i;
-    a[32] = r0_i[31] & signed_ext;
-    b[31:0] = r1_i;
-    b[32] = r1_i[31] & signed_ext;
+// EX-M1 stage: do 4 multiply
+(* use_dsp = "yes" *) logic[47:0] l_q;
+(* use_dsp = "yes" *) logic[63:16] h_q;
+logic[63:32] h_fix_q;
+always_ff@(posedge clk) begin
+  if(!m1_stall_i) begin
+    l_q <= a[15: 0] * b[31:0];
+    h_q <= a[31:16] * b[31:0];
+    h_fix_q <= h_fix;
+    get_high_m1 <= get_high;
   end
+end
 
-  always @(posedge clk) begin
-    if(!m1_stall_i) begin
-      get_high_q <= get_high;
-      l_q <= a[32:0] * b[15:0];
-      h_q <= a[32:0] * b[32:16];
-    end
+// M1-M2 stage: do sum up
+(* use_dsp = "yes" *) logic[63:0] full_result;
+always_ff @(posedge clk) begin
+  if(!m2_stall_i) begin
+    full_result <= {16'd0,l_q} + {h_q,16'd0} + {h_fix_q,32'd0};
+    get_high_q <= get_high_m1;
   end
+end
 
-  logic[63:16] high_part_result;
-  assign full_result = {{15{l_q[48]}}, l_q[48:16]} + {h_q[63:16]};
-  always @(posedge clk) begin
-    if(!m2_stall_i) begin
-      if(get_high_q) begin
-        result_o <= high_part_result[63:32];
-      end
-      else begin
-        result_o <= l_q[31:0] + {h_q[31:16], 16'd0};
-      end
-    end
-  end
+// OUTPUT LOGIC
+assign result_o = get_high_q ? full_result[63:32] : full_result[31:0];
 
 endmodule
